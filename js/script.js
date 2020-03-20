970d3e402720b70a07c04d06c30d768e0cf17ad3
bitlowsky <bitlowsky@gmail.com>
2020-03-20 23:36:28 +0300

    Fix animation & control page loading

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 document.getElementById('body').style.backgroundColor = getRandomColor()
-getGitProfile("bitlowsky");
+// getGitProfile("bitlowsky");
+belay();
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -24,3 +25,25 @@ async function getGitProfile(userName) {
         document.getElementById("git-location").innerText = data.location;
     }
 };
+
+function belay() {
+    let test = document.getElementById("git-avatar").getAttribute("src");
+    if (!test) {
+        let data = {
+            "login": "bitlowsky",
+            "avatar_url": "https://avatars2.githubusercontent.com/u/55452635?v=4",
+            "url": "https://api.github.com/users/bitlowsky",
+            "html_url": "https://github.com/bitlowsky",
+            "name": "Dmitry Pigasin",
+            "location": "Nizhny Novgorod, Russia",
+            "bio": "I'm student",
+        };
+        document.getElementById("git-avatar").src = data.avatar_url;
+        document.getElementById("git-name").innerText = data.name;
+        document.getElementById("git-username").innerText = data.login;
+
+        if (data.location) {
+            document.getElementById("git-location").innerText = data.location;
+        }
+    }
+}
