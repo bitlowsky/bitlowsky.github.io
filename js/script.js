3e6bb6ff4cb29839fd5ada50cff0af07b57a7cef
bitlowsky <bitlowsky@gmail.com>
2020-03-22 01:22:25 +0300

    Update style: project is <a> now

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 "use strict";
 
 document.getElementById('body').style.backgroundColor = getRandomColor()
-// getGitProfile("bitlowsky");
-belay();
+
+getGitProfile("bitlowsky");
+
+if (!document.getElementById("git-avatar").getAttribute("src")) {
+    fallback();
+}
+
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -26,24 +31,23 @@ async function getGitProfile(userName) {
     }
 };
 
-function belay() {
-    let test = document.getElementById("git-avatar").getAttribute("src");
-    if (!test) {
-        let data = {
-            "login": "bitlowsky",
-            "avatar_url": "https://avatars2.githubusercontent.com/u/55452635?v=4",
-            "url": "https://api.github.com/users/bitlowsky",
-            "html_url": "https://github.com/bitlowsky",
-            "name": "Dmitry Pigasin",
-            "location": "Nizhny Novgorod, Russia",
-            "bio": "I'm student",
-        };
-        document.getElementById("git-avatar").src = data.avatar_url;
-        document.getElementById("git-name").innerText = data.name;
-        document.getElementById("git-username").innerText = data.login;
+function fallback() {
+    let data = {
+        "login": "bitlowsky",
+        "avatar_url": "https://avatars2.githubusercontent.com/u/55452635?v=4",
+        "url": "https://api.github.com/users/bitlowsky",
+        "html_url": "https://github.com/bitlowsky",
+        "name": "Dmitry Pigasin",
+        "location": "Nizhny Novgorod, Russia",
+        "bio": "I'm student",
+        "blog": "https://bitlowsky.github.io"
+    };
 
-        if (data.location) {
-            document.getElementById("git-location").innerText = data.location;
-        }
+    document.getElementById("git-avatar").src = data.avatar_url;
+    document.getElementById("git-name").innerText = data.name;
+    document.getElementById("git-username").innerText = data.login;
+
+    if (data.location) {
+        document.getElementById("git-location").innerText = data.location;
     }
 }
